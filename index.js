45774b6007dde3178ac3b8b9fb7b35ecd813dddb
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-08-31 08:37:46 -0300

    imprime links en la consol

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 #! /usr/bin/env node
-const mdLinks = require('mdLinks');
+const mdLinks = require('./lib/md-links').mdLinks;
+const path = require('path');
+const colors = require('colors');
 
+if (require.main === module) {
+  const [, , ...args] = process.argv;
+  let options = {};
+  if (args.includes('--validate')) options.validate = true;
+  if (args.includes('--stats')) options.stats = true;
+
+  mdLinks(path.join(process.cwd(), args[0]), options).then((links) => {
+    links.forEach(link => {
+      let result = '';
+      if (options.validate) result = 
+        `${link.file} - line:${link.line} - ${colors.green(link.href)} - ${link.text} - ${link.ok} - ${link.status}`;
+      else result = 
+        `${link.file} - line:${link.line} : ${colors.green(link.href)} : ${link.text}`;
+      console.log(result);
+    });
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+module.exports = mdLinks;
